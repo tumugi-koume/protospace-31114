15dc3ccab679f0b5362e3c247ecf3683e1a1b6fd
tumugi-koume <le24.juin@ezweb.ne.jp>
2020-10-27 10:55:16 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  devise_for :users
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'prototypes#index'
+  resources :users,      only: [:index, :new, :create, :show]
+  resources :prototypes do
+    resources :comments,   only: [:create]
+  end
+end
